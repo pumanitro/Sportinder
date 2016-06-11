7b77bd78c2c88034262561374784931c8e780d0f
miko94 <m.klosowski94@gmail.com>
2016-06-11 02:01:57 +0200

    Added hamburger

--- Sportinder/App.xaml.cs
+++ Sportinder/App.xaml.cs
@@ -39,12 +39,6 @@ namespace Sportinder
         /// <param name="e">Details about the launch request and process.</param>
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
-#if DEBUG
-            if (System.Diagnostics.Debugger.IsAttached)
-            {
-                this.DebugSettings.EnableFrameRateCounter = true;
-            }
-#endif
             Frame rootFrame = Window.Current.Content as Frame;
 
             // Do not repeat app initialization when the Window already has content,
@@ -72,7 +66,7 @@ namespace Sportinder
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(Login), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
--- Sportinder/MainPage.xaml.cs
+++ Sportinder/MainPage.xaml.cs
@@ -25,6 +25,59 @@ namespace Sportinder
         public MainPage()
         {
             this.InitializeComponent();
+            BackButton.Visibility = Visibility.Collapsed;
+            MyFrame.Navigate(typeof(HomePage));
+            PageName.Text = "Home Page";
+            HomePage.IsSelected = true;
+
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MyFrame.CanGoBack)
+            {
+                MyFrame.GoBack();
+                HomePage.IsSelected = true;
+                MySplitView.IsPaneOpen = false;
+            }
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+  
+            if (HomePage.IsSelected)
+            {
+                BackButton.Visibility = Visibility.Collapsed;
+                MyFrame.Navigate(typeof(HomePage));
+                PageName.Text = "Home Page";
+                MySplitView.IsPaneOpen = false;
+            }
+            else if (Settings.IsSelected)
+            {
+                BackButton.Visibility = Visibility.Visible;
+                MyFrame.Navigate(typeof(Settings));
+                PageName.Text = "Settings";
+                MySplitView.IsPaneOpen = false;
+            }
+            else if (EventsAttended.IsSelected)
+            {
+                BackButton.Visibility = Visibility.Visible;
+                MyFrame.Navigate(typeof(EventsAttended));
+                PageName.Text = "My Events";
+                MySplitView.IsPaneOpen = false;
+            }
+            else if (EventsCreated.IsSelected)
+            {
+                BackButton.Visibility = Visibility.Visible;
+                MyFrame.Navigate(typeof(EventsCreated));
+                PageName.Text = "Events Created";
+                MySplitView.IsPaneOpen = false;
+            }
         }
     }
 }
